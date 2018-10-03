f9750d7115a99702d3fc6e69b848a4adc7d941e8
vlad <vraykovsky2016@gmail.com>
2018-10-03 05:35:26 +0300

    Initial commit

---
+++ src/main/java/com/adminrocket/adminrocket/AdminRocketApplication.java
@@ -0,0 +1,12 @@
+package com.adminrocket.adminrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminRocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminRocketApplication.class, args);
+    }
+}
---
+++ src/test/java/com/adminrocket/adminrocket/AdminRocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adminrocket.adminrocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminRocketApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
